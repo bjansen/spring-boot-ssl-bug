cae8de1aa8823a47ae1a0045504edcc1b54b32e6
Bastien JANSEN <bastien.jansen@gmx.com>
2023-10-18 11:21:13 +0200

    Reproduce bug

---
+++ src/main/java/com/example/demossl/DemoSslApplication.java
@@ -0,0 +1,13 @@
+package com.example.demossl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSslApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSslApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demossl/DemoSslApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demossl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSslApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
